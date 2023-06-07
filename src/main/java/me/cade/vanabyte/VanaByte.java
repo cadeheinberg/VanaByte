2d8f876d4fd5b0fcd700b05011940679c5a2d7f9
cadeheinberg <62829015+cadeheinberg@users.noreply.github.com>
2023-06-07 00:05:01 -0500

    test

---
+++ src/main/java/me/cade/vanabyte/VanaByte.java
@@ -0,0 +1,17 @@
+package me.cade.vanabyte;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class VanaByte extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
